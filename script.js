660b59bd11bff58438b60c1521b72910df62bbf5
Pswie <langellotti19@live.it>
2023-08-25 23:10:14 +0200

    w

--- script.js
+++ script.js
@@ -7,17 +7,12 @@ let currentSlide = 0; // Indice dell'immagine corrente
 
 // Funzione per aggiornare la visualizzazione del carosello
 function updateSlide() {
-    // Per desktop
     carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
     indicator.style.transform = `translateX(${currentSlide * 50}px)`;
 
-    // Rimuovi la classe 'active' da tutti i pulsanti
     buttons.forEach(button => button.classList.remove('active'));
-
-    // Aggiungi la classe 'active' al pulsante corrispondente all'immagine corrente
     buttons[currentSlide].classList.add('active');
 
-    // Per mobile
     mobileImages.forEach((img, index) => {
         if (index === currentSlide) {
             img.classList.add('active');
@@ -27,36 +22,22 @@ function updateSlide() {
     });
 }
 
-// Aggiunge un evento di click a ogni pallino
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        currentSlide = index; // Imposta l'indice dell'immagine corrente
-        updateSlide(); // Aggiorna la visualizzazione del carosello
+        currentSlide = index;
+        updateSlide();
     });
 });
 
-// Funzione per lo scorrimento automatico delle immagini
 setInterval(() => {
-    currentSlide = (currentSlide + 1) % 5; // Incrementa l'indice dell'immagine corrente e torna a 0 dopo aver raggiunto l'ultima immagine
-    updateSlide(); // Aggiorna la visualizzazione del carosello
-}, 7000); // Cambia immagine ogni 7 secondi
+    currentSlide = (currentSlide + 1) % 5;
+    updateSlide();
+}, 7000);
 
-// Funzione per mostrare/nascondere il menu su mobile
-function toggleMenu() {
-    const header = document.querySelector('.header');
-    if (header.style.display === 'none' || header.style.display === '') {
-        header.style.display = 'flex';
-    } else {
-        header.style.display = 'none';
-    }
-}
-
-// Aggiungi l'evento di click al logo per mostrare/nascondere il menu su mobile
-document.querySelector('.logo').addEventListener('click', toggleMenu);
-
-// Gestione Menu ad hamburger
-function toggleMenu() {
+// Funzione per mostrare/nascondere il menu a tendina su mobile
+function toggleMenu(event) {
     if (window.innerWidth <= 600) {
+        event.preventDefault(); // Previene il comportamento predefinito del link
         const dropdown = document.querySelector('.dropdown-menu');
         if (dropdown.style.display === 'block') {
             dropdown.style.display = 'none';
@@ -65,3 +46,8 @@ function toggleMenu() {
         }
     }
 }
+
+// Aggiungi l'evento di click al logo e all'icona hamburger per mostrare/nascondere il menu su mobile
+document.querySelector('.logo').addEventListener('click', toggleMenu);
+document.querySelector('.hamburger-icon').addEventListener('click', toggleMenu);
+
